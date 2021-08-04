319cf81c9185704697f16bf435c4a1fd6a2ed44b
kx8058 <kx8058@g.rit.edu>
2021-08-03 23:59:50 -0400

    Added function for if user enters 'menu'

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
 import json
 import requests
+# import ../config/data.py   # line below might be a better candidate 
+import data.py
+
+def picked_menu():
+	menu_choice = input("You have picked 'menu'. Here are the next options:")
+	print("* Enter '1' to view all of the tickets.")
+	print("* Enter '2' to view a ticket.")
+	print("* Enter 'quit' to exit.")
+
+	if menu_choice != '1' and menu_choice != '2' and menu_choice != 'quit':
+		print("Sorry, I don't think you entered one of the given choices. Please try again.")
+	elif menu_choice == '1':
+		# show all tickets
+	elif menu_choice == '2': 
+		# show a ticket
+	else:
+		print("You have picked 'quit'. Thank you for using the viewer. Goodbye!")
 
 def main():
 	print("Hello, welcome to the ticket viewer!")
-	opening_choice = input("What would you like to do? To view the options, type 'menu'. To exit, type 'quit'.")
+	opening_choice = input("What would you like to do? To view the options, enter 'menu'. To exit, enter 'quit'.")
 	if opening_choice != "menu" and opening_choice != "quit":
 		print("Sorry, I don't think you entered one of the given choices. Please try again.")
 	elif opening_choice == "menu":
